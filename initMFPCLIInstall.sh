d50570234026c75c0eb7946a0a210c0a72d0369d
jojustin@in.ibm.com <jojustin@in.ibm.com>
2015-06-17 14:21:21 +0530

    tt

--- initMFPCLIInstall.sh
+++ initMFPCLIInstall.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+
 #wget http://public.dhe.ibm.com/ibmdl/export/pub/software/products/en/MobileFirstPlatform/mobilefirst_cli_installer_7.0.0.zip
 wget https://www.dropbox.com/s/dtrm0mhm26zl53x/mobilefirst-cli-installer-7.1.0-20150601-0314.zip
 unzip mobilefirst-cli-installer-7.1.0-20150601-0314
@@ -6,22 +6,22 @@ echo "---------- current folder is ------"
 pwd
 
 echo "Before moving to mobile first folder"
-mkdir Worklight-CLI
+#mkdir Worklight-CLI
 ls -la
 cp installer.properties mobilefirst-cli-installer-7.1.0/
 cd mobilefirst-cli-installer-7.1.0
 
-ls -la
+#ls -la
 
 
-cat installer.properties
+#cat installer.properties
 echo "Going to install"
 export LAX_DEBUG=true
 #./install_linux.bin -i silent -Xms1024m -Xmx4096m
 ./install_linux.bin -i silent
 
 ls -la
-echo "install complete"
+
 
 mfp -v
 
