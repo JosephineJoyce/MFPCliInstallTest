3d7215ce6080053234bdcb01e5819b4337f40a4b
jojustin@in.ibm.com <jojustin@in.ibm.com>
2015-06-17 09:53:38 +0530

    Test

--- initMFPCLIInstall.sh
+++ initMFPCLIInstall.sh
@@ -3,6 +3,10 @@
 wget https://www.dropbox.com/s/dtrm0mhm26zl53x/mobilefirst-cli-installer-7.1.0-20150601-0314.zip
 
 java -version
+echo $JAVA_HOME
+
+which java
+
 unzip mobilefirst-cli-installer-7.1.0-20150601-0314
 echo "Before moving to mobile first folder"
 ls -la
