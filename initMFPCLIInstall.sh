ddcc7c3e0239acdfe00676f98e45b57b7d3adf1d
jojustin@in.ibm.com <jojustin@in.ibm.com>
2015-06-17 09:21:37 +0530

    Test

--- initMFPCLIInstall.sh
+++ initMFPCLIInstall.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 #wget http://public.dhe.ibm.com/ibmdl/export/pub/software/products/en/MobileFirstPlatform/mobilefirst_cli_installer_7.0.0.zip
-##wget https://www.dropbox.com/s/dtrm0mhm26zl53x/mobilefirst-cli-installer-7.1.0-20150601-0314.zip
-##wget http://halpert.austin.ibm.com/productionBuilds-capella/LATEST-INTEGRATION/mobilefirst-cli-installer-7.1.0-20150611-0712.zip
-##unzip mobilefirst-cli-installer-7.1.0-20150601-0314
+wget https://www.dropbox.com/s/dtrm0mhm26zl53x/mobilefirst-cli-installer-7.1.0-20150601-0314.zip
+
+unzip mobilefirst-cli-installer-7.1.0-20150601-0314
 echo "Before moving to mobile first folder"
+ls -la
+cp installer.properties mobilefirst-cli-installer-7.1.0/
+cd mobilefirst-cli-installer-7.1.0
+
+ls -la
+
+
+cat installer.properties
+echo "Going to install"
+./install_linux.bin -i silent
+
+echo "install complete"
+
+mfp -v
+
+##cd ..
+##ls -la
 
 
