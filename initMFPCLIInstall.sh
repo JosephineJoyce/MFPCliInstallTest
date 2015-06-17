6879ee83708b94042741aac63f787e51bddc67f3
jojustin@in.ibm.com <jojustin@in.ibm.com>
2015-06-17 18:54:28 +0530

    Test

--- initMFPCLIInstall.sh
+++ initMFPCLIInstall.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
-export LAX_DEBUG=true
-
 #wget http://public.dhe.ibm.com/ibmdl/export/pub/software/products/en/MobileFirstPlatform/mobilefirst_cli_installer_7.0.0.zip
-sudo apt-get https://www.dropbox.com/s/dtrm0mhm26zl53x/mobilefirst-cli-installer-7.1.0-20150601-0314.zip
+wget https://www.dropbox.com/s/dtrm0mhm26zl53x/mobilefirst-cli-installer-7.1.0-20150601-0314.zip
+export LAX_DEBUG=true
+unzip mobilefirst-cli-installer-7.1.0-20150601-0314
+echo "---------- current folder is ------"
+pwd
+
+echo "Before moving to mobile first folder"
+mkdir Worklight-CLI
+ls -la
+cp installer.properties mobilefirst-cli-installer-7.1.0/
+cd mobilefirst-cli-installer-7.1.0
+
+echo "Going to install"
+
+#echo "sudo test"
+#sudo apt-get update &> /dev/null
+#sudo apt-get -y install python2.7 &> /dev/null
+#echo "sudo test end"
+
+#./install_linux.bin -i silent -Xms1024m -Xmx4096m
+sudo install_linux.bin -i silent
+ls -la
+
+echo "To print mfp version" -
+mfp -v
 
 
