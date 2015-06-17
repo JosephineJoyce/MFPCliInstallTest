61e9d49243fc857d74d7d65f6ed0ff8c7f910cf7
jojustin@in.ibm.com <jojustin@in.ibm.com>
2015-06-17 15:33:26 +0530

    test

--- initMFPCLIInstall.sh
+++ initMFPCLIInstall.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #wget http://public.dhe.ibm.com/ibmdl/export/pub/software/products/en/MobileFirstPlatform/mobilefirst_cli_installer_7.0.0.zip
 wget https://www.dropbox.com/s/dtrm0mhm26zl53x/mobilefirst-cli-installer-7.1.0-20150601-0314.zip
+
 unzip mobilefirst-cli-installer-7.1.0-20150601-0314
 echo "---------- current folder is ------"
 pwd
@@ -17,8 +18,10 @@ cd mobilefirst-cli-installer-7.1.0
 #cat installer.properties
 echo "Going to install"
 export LAX_DEBUG=true
+sudo apt-get update &> /dev/null
+sudo apt-get -y install python2.7 &> /dev/null
 #./install_linux.bin -i silent -Xms1024m -Xmx4096m
-sudo ./install_linux.bin -i silent > /dev/null 
+./install_linux.bin -i silent
 
 #ls -la
 
