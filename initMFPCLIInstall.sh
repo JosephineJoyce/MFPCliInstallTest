28d99386cd0e5f4f237052407cf0aca6f6964bda
jojustin@in.ibm.com <jojustin@in.ibm.com>
2015-06-17 10:52:44 +0530

    correction

--- initMFPCLIInstall.sh
+++ initMFPCLIInstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #wget http://public.dhe.ibm.com/ibmdl/export/pub/software/products/en/MobileFirstPlatform/mobilefirst_cli_installer_7.0.0.zip
-#wget https://www.dropbox.com/s/dtrm0mhm26zl53x/mobilefirst-cli-installer-7.1.0-20150601-0314.zip
+wget https://www.dropbox.com/s/dtrm0mhm26zl53x/mobilefirst-cli-installer-7.1.0-20150601-0314.zip
 
 java -version
 echo $JAVA_HOME
@@ -20,6 +20,7 @@ cat installer.properties
 echo "Going to install"
 ./install_linux.bin -i silent -r response.txt
 
+ls -la
 echo "install complete"
 
 mfp -v
